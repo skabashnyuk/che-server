3b1e49313cb84270cb68ab91800e4a9592a85408
skabashnyuk <ksmster@gmail.com>
2018-04-10 14:55:09 +0000

    Generated code by SwaggerHub

--- jaxrs-spec/src/gen/java/io/swagger/api/ServiceApi.java
+++ jaxrs-spec/src/gen/java/io/swagger/api/ServiceApi.java
@@ -15,21 +15,8 @@ import javax.validation.Valid;
 
 @Path("/service")
 @Api(description = "the service API")
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2018-04-10T12:18:12.265Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2018-04-10T14:55:09.998Z")
 public class ServiceApi {
-
-    @POST
-    @Consumes({ "application/json", "application/x-yaml" })
-    @Produces({ "application/json", "application/x-yaml" })
-    @ApiOperation(value = "Create a new CheService", notes = "Create a new CheService", response = CheService.class, tags={  })
-    @ApiResponses(value = { 
-        @ApiResponse(code = 201, message = "The CheService successfully created", response = CheService.class),
-        @ApiResponse(code = 400, message = "400 Server receives invalid input parameter", response = Error.class),
-        @ApiResponse(code = 403, message = "403 The user does not have access to update resource", response = Error.class),
-        @ApiResponse(code = 409, message = "409 Operation could not be performed because of conflict with prior state.", response = Error.class) })
-    public Response addCheService(@Valid CheService cheServiceItem) {
-        return Response.ok().entity("magic!").build();
-    }
 
     @DELETE
     @Path("/{name}/{version}")
@@ -41,6 +28,16 @@ public class ServiceApi {
         @ApiResponse(code = 404, message = "404 The specified resource was not found", response = Error.class),
         @ApiResponse(code = 500, message = "500 Internal server error occurred", response = Error.class) })
     public Response deleteServiceByNameAndVersion(@PathParam("name") @ApiParam("Service name.") String name,@PathParam("version") @ApiParam("Service version.") String version) {
+        return Response.ok().entity("magic!").build();
+    }
+
+    @GET
+    @Produces({ "application/x-yaml", "application/json" })
+    @ApiOperation(value = "Searches CheService by list of ids", notes = "Searches  CheService by list of ids", response = CheService.class, tags={  })
+    @ApiResponses(value = { 
+        @ApiResponse(code = 200, message = "The CheService successfully fetched", response = CheService.class),
+        @ApiResponse(code = 500, message = "500 Internal server error occurred", response = Error.class) })
+    public Response getServiceByIdList(@QueryParam("id") @NotNull   @ApiParam("list if CheService ids.")  List<String> id) {
         return Response.ok().entity("magic!").build();
     }
 
