1ba7c1da28d42b0ba88ee24cf828184b4eec76fa
skabashnyuk <ksmster@gmail.com>
2018-04-10 11:21:38 +0000

    Generated code by SwaggerHub

---
+++ go-server/go/default.go
@@ -0,0 +1,55 @@
+package 
+
+import (
+	"net/http"
+)
+
+type Default struct {
+
+}
+
+func AddCheService(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func AddFeature(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func DeleteFeatureByNameAndVersion(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func DeleteServiceByNameAndVersion(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func GetServiceByName(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func GetServiceByNameAndVersion(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func SearchFeatureByName(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func SearchFeatureByNameAndVersion(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
+func SearchServicesByFeatureList(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+}
+
