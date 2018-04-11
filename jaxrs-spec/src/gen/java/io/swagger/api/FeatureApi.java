40ba822f8088f5f76e0e672313f347724817871b
skabashnyuk <ksmster@gmail.com>
2018-04-11 07:09:13 +0000

    Generated code by SwaggerHub

--- jaxrs-spec/src/gen/java/io/swagger/api/FeatureApi.java
+++ jaxrs-spec/src/gen/java/io/swagger/api/FeatureApi.java
@@ -15,7 +15,7 @@ import javax.validation.Valid;
 
 @Path("/feature")
 @Api(description = "the feature API")
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2018-04-11T07:07:22.788Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2018-04-11T07:09:12.271Z")
 public class FeatureApi {
 
     @POST
